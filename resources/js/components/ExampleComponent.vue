0dee4e34a7629bced74dae9cb4d87bebfaa7452d
Fernanda <ffueltala@gmail.com>
2021-05-29 16:17:39 -0500

    primer commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Cuestionario</div>
+
+                    <div class="card-body">
+                        Pregunta 1
+                    </div>
+                    <div class="card-body">
+                        Pregunta 2
+                    </div>
+                    <div class="card-body">
+                        Pregunta 3
+                    </div>
+                    <div class="card-body">
+                        Pregunta 4
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
