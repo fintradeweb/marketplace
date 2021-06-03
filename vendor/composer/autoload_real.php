3ab27a2ab51b18313f084693f8b9c35c9dec3a41
mfj_correo <mfj_correo@yahoo.com>
2021-06-03 16:32:42 -0500

    se suben cambios en cadena de conexión y se empieza la migración de clients

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit26b2bab06aa88c678faa5efc0c43a0a4
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit26b2bab06aa88c678faa5efc0c43a0a4', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit26b2bab06aa88c678faa5efc0c43a0a4', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit26b2bab06aa88c678faa5efc0c43a0a4::getInitializer($loader));
         } else {
