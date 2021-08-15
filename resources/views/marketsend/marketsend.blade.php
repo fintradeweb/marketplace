7494114fb3f2184ba9603e8ad59a5cd8bb952de8
fintradeweb <85409971+fintradeweb@users.noreply.github.com>
2021-08-14 21:57:34 -0500

    Add files via upload

--- resources/views/marketsend/marketsend.blade.php
+++ resources/views/marketsend/marketsend.blade.php
@@ -1,23 +1,21 @@
 <!doctype html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-    <title>Llamado de emergencia</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+  <title>MarketPlace</title>
 </head>
 <body>
-    <p>Hola! Se ha creado el usuario en MarketPlace  {{ $usuarioCall->created_at }}.</p>
-    <p>Estos son los datos del usuario para acceder en la plataforma:</p>
-    <ul>
-        <li>Email: {{ $usuarioCall->email }}</li>
-        <li>Password: {{ $usuarioCall->password }}</li>
-      </ul>
-     <ul>
-        <li>
-            <a href="http://protomarket.fintrade-acf.com/">
-                Ingresa a nuestra plataforma
-            </a>
-        </li>
-    </ul>
+  <p>Welcome! Your user has been created in MarketPlace  {{ $usuarioCall->name }}.</p>
+  <p>These are the user's data to access the platform:</p>
+  <ul>
+    <li>Email: {{ $usuarioCall->email }}</li>
+    <li>Password: {{ $usuarioCall->password }}</li>
+  </ul>
+  <ul>
+    <li>
+      <a href="http://protomarket.fintrade-acf.com/">Enter our platform</a>
+    </li>
+  </ul>
 </body>
 </html>
