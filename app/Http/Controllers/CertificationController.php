9adb85564084d1bb7b9ab9165e42f71ce7640dff
Fernanda <ffueltala@gmail.com>
2021-09-28 17:31:25 -0500

    cambios 28-09-2021

--- app/Http/Controllers/CertificationController.php
+++ app/Http/Controllers/CertificationController.php
@@ -9,25 +9,26 @@ use DB;
 
 class CertificationController extends Controller{
 
-    public function index(Request $request){
-        $indiv = \App\Models\Certification::consulta_todos( $request->input('email'),  $request->input('token'));
-        $indiv_new = new \App\Models\Certification();
-        if($indiv[0][0]->existe==0){
-            return view('certification.create',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv_new
-            ]);
-        }
-        else{
-            return view('certification.edit',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv[1][0]
-            ]);
-        }
+  public function index(Request $request){
+    $indiv = \App\Models\Certification::consulta_todos( $request->input('email'),  $request->input('token'));
+    $indiv_new = new \App\Models\Certification();
+    if($indiv[0][0]->existe==0){
+      return view('certification.create',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv' =>  $indiv_new
+      ]);
     }
-    public function create(){
+    else{
+      return view('certification.edit',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv' =>  $indiv[1][0]
+      ]);
+    }
+  }
+
+  public function create(){
     return view('certification.create');
   }
 
@@ -37,104 +38,89 @@ class CertificationController extends Controller{
   }
 
   public function store(Request $request){
-    $validator = Validator::make($request->all(), [
+    try{
+      $validator = Validator::make($request->all(), [
+          'approved_agreed' => 'required',
+          'name' => 'required',
+          'title' => 'required',
+          'emf_number_clients'
+      ]);
+      $approved_agreed="";
+      if(!empty($request->input('approved_agreed'))){
+          $approved_agreed =" checked";
+      }
+      $indiv = new \App\Models\Certification();
+      $indiv->approved_agreed =  $approved_agreed;
+      $indiv->name =  $request->input('name');
+      $indiv->title =  $request->input('title');
+      if ($validator->fails()) {
+        throw new \Exception($validator);
+      }
+      $result = \App\Models\Certification::registrar($request);
+      if($result->_error==1){
+        throw new \Exception('There was an error saving the Certification information!');
+      }
+      else{
+        $indiv = \App\Models\Certification::consulta_todos( $request->input('email'),  $request->input('token'));
+        $indiv_new = new \App\Models\Certification();
+        if($indiv[0][0]->existe==0){
+          return view('certification.create',[
+            'email' =>$request->input('email'),
+            'token' =>  $request->input('token'),
+            'indiv' =>  $indiv_new
+          ]);
+        }
+        else{
+          return view('certification.edit',[
+            'email' =>$request->input('email'),
+            'token' =>  $request->input('token'),
+            'indiv' =>  $indiv[1][0]
+          ]);
+        }
+      }
+    }
+    catch(\Exception $e){   
+      return view('certification.create',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv'=>$indiv
+      ])->withErrors($e->getMessage());
+    }      
+  }
+
+  public function update(Request $request, $codigo){
+    try{
+      $validator = Validator::make($request->all(), [
         'approved_agreed' => 'required',
         'name' => 'required',
         'title' => 'required',
         'emf_number_clients'
-    ]);
-    $approved_agreed="";
-    if(!empty($request->input('approved_agreed'))){
+      ]);
+      $approved_agreed="";
+      if(!empty($request->input('approved_agreed'))){
         $approved_agreed =" checked";
-    }
-    $indiv = new \App\Models\Certification();
-    $indiv->approved_agreed =  $approved_agreed;
-    $indiv->name =  $request->input('name');
-    $indiv->title =  $request->input('title');
-    if ($validator->fails()) {
-        return view('certification.create',[
-            'email' =>$request->input('email'),
-            'token' =>  $request->input('token'),
-            'indiv'=>$indiv
-
-        ])->withErrors($validator);
-    }
-    $result = \App\Models\Certification::registrar($request);
-    if($result->_error==1){
-      return view('certification.create',[
-        'email' =>$request->input('email'),
-        'token' =>  $request->input('token'),
-        'indiv'=>$indiv
-      ])->withErrors('There was an error creating the Certification!');
-    }
-    else{
-        $indiv = \App\Models\Certification::consulta_todos( $request->input('email'),  $request->input('token'));
-        $indiv_new = new \App\Models\Certification();
-        if($indiv[0][0]->existe==0){
-            return view('certification.create',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv_new
-            ]);
-        }
-        else{
-            return view('certification.edit',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv[1][0]
-            ]);
-        }
-    }
-  }
-
-  public function update(Request $request, $codigo){
-    $validator = Validator::make($request->all(), [
-      'approved_agreed' => 'required',
-      'name' => 'required',
-      'title' => 'required',
-      'emf_number_clients'
-    ]);
-    $approved_agreed="";
-    if(!empty($request->input('approved_agreed'))){
-      $approved_agreed =" checked";
-    }
-    $indiv = new \App\Models\Certification();
-    $indiv->approved_agreed =  $approved_agreed;
-    $indiv->name =  $request->input('name');
-    $indiv->title =  $request->input('title');
-    if ($validator->fails()) {
-      return view('certification.create',[
-        'email' =>$request->input('email'),
-        'token' =>  $request->input('token'),
-        'indiv'=>$indiv
-      ])->withErrors($validator);
-    }
-    $result = \App\Models\Certification::actualizar($request, $codigo);
-    if($result->_error==1){
-      return view('certification.create',[
-            'email' =>$request->input('email'),
-            'token' =>  $request->input('token'),
-            'indiv'=>$indiv
-      ])->withErrors('There was an error creating the Certification!');
-    }
-    else{
-      return view('certification.final');
-      /*$indiv = \App\Models\Certification::consulta_todos( $request->input('email'),$request->input('token'));
-      $indiv_new = new \App\Models\Certification();
-      if($indiv[0][0]->existe==0){
-        return view('certification.create',[
-          'email' =>$request->input('email'),
-          'token' =>  $request->input('token'),
-          'indiv' =>  $indiv_new
-        ]);
+      }
+      $indiv = new \App\Models\Certification();
+      $indiv->approved_agreed =  $approved_agreed;
+      $indiv->name =  $request->input('name');
+      $indiv->title =  $request->input('title');
+      if ($validator->fails()) {
+        throw new \Exception($validator);
+      }
+      $result = \App\Models\Certification::actualizar($request, $codigo);
+      if($result->_error==1){
+        throw new \Exception('There was an error creating the Certification!');        
       }
       else{
-        return view('certification.edit',[
-          'email' =>$request->input('email'),
-          'token' =>  $request->input('token'),
-          'indiv' =>  $indiv[1][0]
-        ]);
-      }*/
+        return view('certification.final');     
+      }
+    }
+    catch(\Exception $e){   
+      return view('certification.create',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv'=>$indiv
+      ])->withErrors($e->getMessage());
     }
   }
 }
--- app/Http/Controllers/FinancialController.php
+++ app/Http/Controllers/FinancialController.php
@@ -9,25 +9,24 @@ use DB;
 
 class FinancialController extends Controller{
 
-    public function index(Request $request){
-        $indiv = \App\Models\Financial::consulta_todos( $request->input('email'),  $request->input('token'));
-        $indiv_new = new \App\Models\Financial();
-        if($indiv[0][0]->existe==0){
-            return view('financial.create',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv_new
-            ]);
-        }
-        else{
-            return view('financial.edit',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv[1][0]
-            ]);
-        }
+  public function index(Request $request){
+    $indiv = \App\Models\Financial::consulta_todos( $request->input('email'),  $request->input('token'));
+    $indiv_new = new \App\Models\Financial();
+    if($indiv[0][0]->existe==0){
+      return view('financial.create',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv' =>  $indiv_new
+      ]);
     }
-
+    else{
+      return view('financial.edit',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv' =>  $indiv[1][0]
+      ]);
+    }
+  }
 
   public function edit($id){
     $financial = \App\Models\Financial::consulta($id);
@@ -35,7 +34,8 @@ class FinancialController extends Controller{
   }
 
   public function store(Request $request){
-    $validator = Validator::make($request->all(), [
+    try{
+      $validator = Validator::make($request->all(), [
         'avg_montky_sales' => 'required',
         'ams_how_clients' => 'required',
         'estimated_montly_financing' => 'required',
@@ -46,185 +46,174 @@ class FinancialController extends Controller{
         'in_finance' => 'required_without:po_finance'
       ]);
       $has_applicant = "";
-        $po_finance = "";
-        $in_finance = "";
-        $lawsuits_pending = "";
-        $receivable_finance = "";
-        $credit_insurance_policy = "";
-        $declared_bank_ruptcy = "";
+      $po_finance = "";
+      $in_finance = "";
+      $lawsuits_pending = "";
+      $receivable_finance = "";
+      $credit_insurance_policy = "";
+      $declared_bank_ruptcy = "";
 
-        if(!empty($request->input('has_applicant'))){
-            $has_applicant = " checked";
-        }
-        if(!empty($request->input('po_finance'))){
-            $po_finance =" checked";
-        }
-        if(!empty($request->input('in_finance'))){
-            $in_finance = " checked";
-        }
-        if(!empty($request->input('lawsuits_pending'))){
-            $lawsuits_pending = " checked";
-        }
-        if(!empty($request->input('receivable_finance'))){
-            $receivable_finance = " checked";
-        }
-        if(!empty($request->input('credit_insurance_policy'))){
-            $credit_insurance_policy = " checked";
-        }
-        if(!empty($request->input('declared_bank_ruptcy'))){
-            $declared_bank_ruptcy = " checked";
-        }
-        $indiv = new \App\Models\Financial();
-        $indiv->avg_montky_sales =  $request->input('avg_montky_sales');
-        $indiv->ams_how_clients =  $request->input('ams_how_clients');
-        $indiv->estimated_montly_financing =  $request->input('estimated_montly_financing');
-        $indiv->emf_number_clients =  $request->input('emf_number_clients');
-        $indiv->rf_when_with_whom =  $request->input('rf_when_with_whom');
-        $indiv->cip_when_with_whom =  $request->input('cip_when_with_whom');
-        $indiv->has_applicant =  $has_applicant;
-        $indiv->po_finance =  $po_finance;
-        $indiv->in_finance = $in_finance;
-        $indiv->lawsuits_pending = $lawsuits_pending;
-        $indiv->receivable_finance = $receivable_finance;
-        $indiv->credit_insurance_policy = $credit_insurance_policy;
-        $indiv->declared_bank_ruptcy = $declared_bank_ruptcy;
+      if(!empty($request->input('has_applicant'))){
+        $has_applicant = " checked";
+      }
+      if(!empty($request->input('po_finance'))){
+        $po_finance =" checked";
+      }
+      if(!empty($request->input('in_finance'))){
+        $in_finance = " checked";
+      }
+      if(!empty($request->input('lawsuits_pending'))){
+        $lawsuits_pending = " checked";
+      }
+      if(!empty($request->input('receivable_finance'))){
+        $receivable_finance = " checked";
+      }
+      if(!empty($request->input('credit_insurance_policy'))){
+        $credit_insurance_policy = " checked";
+      }
+      if(!empty($request->input('declared_bank_ruptcy'))){
+        $declared_bank_ruptcy = " checked";
+      }
+      $indiv = new \App\Models\Financial();
+      $indiv->avg_montky_sales =  $request->input('avg_montky_sales');
+      $indiv->ams_how_clients =  $request->input('ams_how_clients');
+      $indiv->estimated_montly_financing =  $request->input('estimated_montly_financing');
+      $indiv->emf_number_clients =  $request->input('emf_number_clients');
+      $indiv->rf_when_with_whom =  $request->input('rf_when_with_whom');
+      $indiv->cip_when_with_whom =  $request->input('cip_when_with_whom');
+      $indiv->has_applicant =  $has_applicant;
+      $indiv->po_finance =  $po_finance;
+      $indiv->in_finance = $in_finance;
+      $indiv->lawsuits_pending = $lawsuits_pending;
+      $indiv->receivable_finance = $receivable_finance;
+      $indiv->credit_insurance_policy = $credit_insurance_policy;
+      $indiv->declared_bank_ruptcy = $declared_bank_ruptcy;
 
       if ($validator->fails()) {
-
-        return view('financial.create',[
-            'email' =>$request->input('email'),
-            'token' =>  $request->input('token'),
-            'indiv'=>$indiv
-
-        ])->withErrors($validator);
+        throw new \Exception($validator);                
       }
 
-    $result = \App\Models\Financial::registrar($request);
-
-    if($result->_error==1){
-        return view('financial.create',[
-            'email' =>$request->input('email'),
-            'token' =>  $request->input('token'),
-            'indiv'=>$indiv
-        ])->withErrors('There was an error creating the financial request!');
-    }
-    else{
+      $result = \App\Models\Financial::registrar($request);      
+      if($result->_error==1){
+        throw new \Exception("There was an error creating the financial request!");                
+      }
+      else{
         $indiv = \App\Models\Bankinformation::consulta_todos( $request->input('email'),  $request->input('token'));
         $indiv_new = new \App\Models\Bankinformation();
         if($indiv[0][0]->existe==0){
-            return view('bankinformation.create',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv_new
-            ]);
+          return view('bankinformation.create',[
+            'email' =>$request->input('email'),
+            'token' =>  $request->input('token'),
+            'indiv' =>  $indiv_new
+          ]);
         }
         else{
-            return view('bankinformation.edit',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv' =>  $indiv[1][0]
-            ]);
+          return view('bankinformation.edit',[
+            'email' =>$request->input('email'),
+            'token' =>  $request->input('token'),
+            'indiv' =>  $indiv[1][0]
+          ]);
         }
+      }
     }
-
-
+    catch(\Exception $e){      
+      return view('financial.create',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv'=>$indiv
+      ])->withErrors($e->getMessage());
+    }
   }
 
-  public function update(Request $request, $codigo)
-  {
-        $validator = Validator::make($request->all(), [
-            'avg_montky_sales' => 'required',
-            'ams_how_clients' => 'required',
-            'estimated_montly_financing' => 'required',
-            'emf_number_clients' => 'required',
-            'rf_when_with_whom' => 'required',
-            'cip_when_with_whom' => 'required',
-            'po_finance' => 'required_without:in_finance',
-            'in_finance' => 'required_without:po_finance'
-        ]);
-        $has_applicant = "";
-        $po_finance = "";
-        $in_finance = "";
-        $lawsuits_pending = "";
-        $receivable_finance = "";
-        $credit_insurance_policy = "";
-        $declared_bank_ruptcy = "";
+  public function update(Request $request, $codigo){
+    try{  
+      $validator = Validator::make($request->all(), [
+        'avg_montky_sales' => 'required',
+        'ams_how_clients' => 'required',
+        'estimated_montly_financing' => 'required',
+        'emf_number_clients' => 'required',
+        'rf_when_with_whom' => 'required',
+        'cip_when_with_whom' => 'required',
+        'po_finance' => 'required_without:in_finance',
+        'in_finance' => 'required_without:po_finance'
+      ]);
+      $has_applicant = "";
+      $po_finance = "";
+      $in_finance = "";
+      $lawsuits_pending = "";
+      $receivable_finance = "";
+      $credit_insurance_policy = "";
+      $declared_bank_ruptcy = "";
 
-        if(!empty($request->input('has_applicant'))){
-            $has_applicant = " checked";
-        }
-        if(!empty($request->input('po_finance'))){
-            $po_finance =" checked";
-        }
-        if(!empty($request->input('in_finance'))){
-            $in_finance = " checked";
-        }
-        if(!empty($request->input('lawsuits_pending'))){
-            $lawsuits_pending = " checked";
-        }
-        if(!empty($request->input('receivable_finance'))){
-            $receivable_finance = " checked";
-        }
-        if(!empty($request->input('credit_insurance_policy'))){
-            $credit_insurance_policy = " checked";
-        }
-        if(!empty($request->input('declared_bank_ruptcy'))){
-            $declared_bank_ruptcy = " checked";
-        }
-        $indiv = new \App\Models\Financial();
-        $indiv->avg_montky_sales =  $request->input('avg_montky_sales');
-        $indiv->ams_how_clients =  $request->input('ams_how_clients');
-        $indiv->estimated_montly_financing =  $request->input('estimated_montly_financing');
-        $indiv->emf_number_clients =  $request->input('emf_number_clients');
-        $indiv->rf_when_with_whom =  $request->input('rf_when_with_whom');
-        $indiv->cip_when_with_whom =  $request->input('cip_when_with_whom');
-        $indiv->has_applicant =  $has_applicant;
-        $indiv->po_finance =  $po_finance;
-        $indiv->in_finance = $in_finance;
-        $indiv->lawsuits_pending = $lawsuits_pending;
-        $indiv->receivable_finance = $receivable_finance;
-        $indiv->credit_insurance_policy = $credit_insurance_policy;
-        $indiv->declared_bank_ruptcy = $declared_bank_ruptcy;
+      if(!empty($request->input('has_applicant'))){
+        $has_applicant = " checked";
+      }
+      if(!empty($request->input('po_finance'))){
+        $po_finance =" checked";
+      }
+      if(!empty($request->input('in_finance'))){
+        $in_finance = " checked";
+      }
+      if(!empty($request->input('lawsuits_pending'))){
+        $lawsuits_pending = " checked";
+      }
+      if(!empty($request->input('receivable_finance'))){
+        $receivable_finance = " checked";
+      }
+      if(!empty($request->input('credit_insurance_policy'))){
+        $credit_insurance_policy = " checked";
+      }
+      if(!empty($request->input('declared_bank_ruptcy'))){
+        $declared_bank_ruptcy = " checked";
+      }
+      $indiv = new \App\Models\Financial();
+      $indiv->avg_montky_sales =  $request->input('avg_montky_sales');
+      $indiv->ams_how_clients =  $request->input('ams_how_clients');
+      $indiv->estimated_montly_financing =  $request->input('estimated_montly_financing');
+      $indiv->emf_number_clients =  $request->input('emf_number_clients');
+      $indiv->rf_when_with_whom =  $request->input('rf_when_with_whom');
+      $indiv->cip_when_with_whom =  $request->input('cip_when_with_whom');
+      $indiv->has_applicant =  $has_applicant;
+      $indiv->po_finance =  $po_finance;
+      $indiv->in_finance = $in_finance;
+      $indiv->lawsuits_pending = $lawsuits_pending;
+      $indiv->receivable_finance = $receivable_finance;
+      $indiv->credit_insurance_policy = $credit_insurance_policy;
+      $indiv->declared_bank_ruptcy = $declared_bank_ruptcy;
 
-        if ($validator->fails()) {
-
-            return view('financial.create',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv'=>$indiv
-
-            ])->withErrors($validator);
+      if ($validator->fails()) {
+        throw new \Exception($validator);            
+      }
+      $result = \App\Models\Financial::actualizar($request, $codigo);
+      if($result->_error==1){
+        throw new \Exception("There was an error creating the financial request!");           
+      }
+      else{
+        $indiv = \App\Models\Bankinformation::consulta_todos( $request->input('email'),  $request->input('token'));
+        $indiv_new = new \App\Models\Bankinformation();
+        if($indiv[0][0]->existe==0){
+          return view('bankinformation.create',[
+            'email' =>$request->input('email'),
+            'token' =>  $request->input('token'),
+            'indiv' =>  $indiv_new
+          ]);
         }
-        $result = \App\Models\Financial::actualizar($request, $codigo);
-        if($result->_error==1)
-        {
-            return view('financial.create',[
-                'email' =>$request->input('email'),
-                'token' =>  $request->input('token'),
-                'indiv'=>$indiv
-            ])->withErrors('There was an error creating the financial request!');
+        else{
+          return view('bankinformation.edit',[
+            'email' =>$request->input('email'),
+            'token' =>  $request->input('token'),
+            'indiv' =>  $indiv[1][0]
+          ]);
         }
-        else
-        {
-            $indiv = \App\Models\Bankinformation::consulta_todos( $request->input('email'),  $request->input('token'));
-            $indiv_new = new \App\Models\Bankinformation();
-            if($indiv[0][0]->existe==0){
-                return view('bankinformation.create',[
-                    'email' =>$request->input('email'),
-                    'token' =>  $request->input('token'),
-                    'indiv' =>  $indiv_new
-                ]);
-            }
-            else{
-                return view('bankinformation.edit',[
-                    'email' =>$request->input('email'),
-                    'token' =>  $request->input('token'),
-                    'indiv' =>  $indiv[1][0]
-                ]);
-            }
-        }
-
+      }
+    }
+    catch(\Exception $e){  
+      return view('financial.create',[
+        'email' =>$request->input('email'),
+        'token' =>  $request->input('token'),
+        'indiv'=>$indiv
+      ])->withErrors($e->getMessage());  
+    }  
   }
-
 
 }
